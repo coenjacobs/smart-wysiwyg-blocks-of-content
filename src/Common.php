27d49e6c858ab31c76d95ff55f974a936064181d
Coen Jacobs <coenjacobs@gmail.com>
2015-05-23 21:42:21 +0100

    register_widgets in a separate method

--- src/Common.php
+++ src/Common.php
@@ -4,7 +4,11 @@ class Common {
 	public function __construct() {
 		new PostTypes();
 
-		add_action( 'widgets_init', create_function( '', "register_widget( 'CoenJacobs\SWBOC\Widget' );" ) );
+		add_action( 'widgets_init', array( $this, 'register_widgets' ) );
+	}
+
+	public function register_widgets() {
+		register_widget( 'CoenJacobs\SWBOC\Widget' );
 	}
 }
 
